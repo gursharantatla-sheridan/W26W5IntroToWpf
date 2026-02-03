126e1a503b81406d70daadeb4b423d9a957b26b7
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-02-03 18:42:46 -0500

    grid eg

---
+++ W26W5IntroToWpf/GridExample.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace W26W5IntroToWpf
+{
+    /// <summary>
+    /// Interaction logic for GridExample.xaml
+    /// </summary>
+    public partial class GridExample : Window
+    {
+        public GridExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
